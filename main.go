22eec94653455edb25056d95dd74e4e80cf25de5
chavda-parth <132228922+chavda-parth@users.noreply.github.com>
2026-01-15 20:00:39 +0530

    WIP: Learn basic widget creation and state update.

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
+
 func main() {
-	myApp := app.New()
-	myWindow := myApp.NewWindow("ADB Test")
+	var app fyne.App = app.New()
+	var window fyne.Window = app.NewWindow("ADB GUI")
 
-	myWindow.SetContent(widget.NewLabel("Hello. Testing ADB GUI"))
+	var clickCounter int = 0
 
-	myWindow.ShowAndRun()
+	var label *widget.Label = widget.NewLabel(fmt.Sprintf("You clicked the button %d times.", clickCounter))
+	var button *widget.Button = widget.NewButton("Click here.", func() {
+		clickCounter++
+		label.SetText(fmt.Sprintf("You clicked the button %d times.", clickCounter))
+	})
+
+	var container *fyne.Container = container.New(
+		layout.NewVBoxLayout(),
+		button,
+		// layout.NewSpacer(),
+		label,
+	)
+
+	window.SetContent(container)
+	window.ShowAndRun()
 }
